98fb7c7ff09275f3d9860cc96990cb0c3e609a12
lizhongcheng <1114979847@qq.com>
2020-08-14 10:45:29 +0800

    部署到私库上

---
+++ base/src/main/java/com/zixiu/base/TestRepositoryUtil.java
@@ -0,0 +1,16 @@
+package com.zixiu.base;
+
+import android.util.Log;
+
+/**
+ * Author: Snail
+ * Time:  2020/8/14 10:04 AM
+ * FileName:  TestRepositoryUtil
+ * 简介：
+ */
+public class TestRepositoryUtil {
+
+    public static void log(String str){
+        Log.d("TestRepositoryUtil",str);
+    }
+}
